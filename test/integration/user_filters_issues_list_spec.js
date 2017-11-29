b3e3c56325ca1696e8b251341730554d0c4efaaf
Fede <federico@abstracta.us>
2017-11-29 15:01:05 -0300

    4 nuevas historias

--- test/integration/user_filters_issues_list_spec.js
+++ test/integration/user_filters_issues_list_spec.js
@@ -13,4 +13,44 @@ describe('issues list', function() {
         cy.contains('lorem ipsum').should('exist');
         cy.contains('foo bar').should('exist');
     });
+    it('filters issues by open status', function(){
+        cy.resetDB();
+        cy.fixture({title: 'issue 1', status:'open'});
+        cy.fixture({title: 'issue 2', status:'open'});
+        cy.fixture({title: 'issue 3', status:'closed'});
+
+        cy.visit('/issues');
+
+        cy.contains('issue 1').should('exist');
+        cy.contains('issue 2').should('exist');
+        cy.contains('issue 3').should('exist');
+
+        cy.get('[data-test-open-filter]')
+            .click();
+        
+        cy.contains('issue 1').should('exist');
+        cy.contains('issue 2').should('exist');
+        cy.contains('issue 3').should('not.exist');
+
+    });
+     it('filters issues by closed status', function(){
+        cy.resetDB();
+        cy.fixture({title: 'issue 1', status:'open'});
+        cy.fixture({title: 'issue 2', status:'open'});
+        cy.fixture({title: 'issue 3', status:'closed'});
+
+        cy.visit('/issues');
+
+        cy.contains('issue 1').should('exist');
+        cy.contains('issue 2').should('exist');
+        cy.contains('issue 3').should('exist');
+
+        cy.contains('Closed issues') //ya me sirve para verificar el texto del link
+            .click();
+        
+        cy.contains('issue 1').should('not.exist');
+        cy.contains('issue 2').should('not.exist');
+        cy.contains('issue 3').should('exist');
+
+    });
 });

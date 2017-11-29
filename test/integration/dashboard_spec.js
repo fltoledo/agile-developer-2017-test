3298b7f48e1a50d46d23abf815bd4f21af77bd2b
Fede <federico@abstracta.us>
2017-11-29 15:46:37 -0300

    total de issues abiertos en dashboard

--- test/integration/dashboard_spec.js
+++ test/integration/dashboard_spec.js
@@ -1,3 +1,19 @@
 describe('Dashboard', function() {
 
+  it('Shows the number of open issues in the Dashboard', function() {
+    cy.resetDB();
+    cy.fixture({
+        status: 'open'
+    });
+    cy.fixture({
+        status: 'open'
+    });
+    cy.fixture({
+        status: 'closed'
+    });
+
+    cy.visit('/dashboard');
+
+    cy.get('[data-test-open-issues]').should('contain', '2 open issues');
+  });
 });
